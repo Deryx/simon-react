d55c3baaa01a21ff614d568da429379e616e1c09
Deryx <deryxescott@gmail.com>
2019-02-11 00:10:30 -0500

    Add game logic to Control Panel component

--- src/components/control-panel/control-panel.tsx
+++ src/components/control-panel/control-panel.tsx
@@ -6,14 +6,58 @@ import StartButton from '../start-button/start-button';
 import StrictButton from '../strict-button/strict-button';
 import './styles.css';
 
+const MAX_STEPS: number = 20;
+const BLANK_COUNTER: string = '';
+const ON_COUNTER: string = '--';
+const WRONG_COUNTER: string = '!!';
+
+let stepNumber = 0;
 let strictState: string = 'off';
 let switchState: string = 'off';
+
+let playerPattern: number[] = [];
+let computerPattern: number[] = [];
 
 function showDoubleDigit( num: number ): any {
     if( num >= 0 && num <= 9 ) {
         return "0" + num;
     } else {
         return num;
+    }
+}
+
+function arraysIdentical( arr1: any, arr2: any ): boolean {
+    if( arr1.length !== arr2.length ) {
+        return false;
+    }
+    for( let i = 0, len = arr1.length; i <= len; i++ ) {
+        if( arr1[i] !== arr2[i] ) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function getRandomButton(): number {
+    return Math.floor( Math.random() * 4 );
+}
+
+function playNextRound() {
+    stepNumber++;
+}
+
+function resetGame(): void {
+    stepNumber = 0;
+    playerPattern = [];
+    computerPattern = [];
+}
+
+function playGame(): any {
+    if( stepNumber < MAX_STEPS ) {
+        stepNumber++;
+    } else {
+        return playGame();
     }
 }
 
@@ -29,6 +73,7 @@ class ControlPanel extends React.Component {
 
         if(switchState && switchState === 'on') {
             patternCounter.textContent = showDoubleDigit(0);
+            playGame();
         }
     }
 
@@ -56,10 +101,10 @@ class ControlPanel extends React.Component {
         switchState  = switchState === 'off' ? 'on' : 'off';
         if(switchState && switchState === 'off') {
             switchButton.style.float = 'left';
-            patternCounter.textContent = '';
+            patternCounter.textContent = BLANK_COUNTER;
         } else {
             switchButton.style.float = 'right';
-            patternCounter.textContent = '--';
+            patternCounter.textContent = ON_COUNTER;
         }
     }
 
