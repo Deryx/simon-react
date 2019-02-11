d560033544519c3fe9c1361233b9ee56fbf968c7
Deryx <deryxescott@gmail.com>
2019-02-10 22:08:38 -0500

    Update On-Off-Switch component

--- src/components/on-off-switch/on-off-switch.tsx
+++ src/components/on-off-switch/on-off-switch.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import './styles.css';
 
-class OnOffSwitch extends React.Component {
+interface IActionProps {
+    switchAction: any
+}
+
+class OnOffSwitch extends React.Component<IActionProps> {
+
     public render() {
         return (
             <div className="row">
                 <div className="off">off</div>
                 <div className="case">
-                    <div className="switch" />
+                    <div className="switch" onClick={this.props.switchAction} />
                 </div>
                 <div className="on">on</div>
             </div>
