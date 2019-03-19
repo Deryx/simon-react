ed42b2709a931ea391ffa46f34965c0c35c2689a
Deryx <deryxescott@gmail.com>
2019-03-19 16:38:22 -0400

    Update Pattern Counter Code

--- src/components/pattern-counter/pattern-counter.tsx
+++ src/components/pattern-counter/pattern-counter.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import './styles.css';
 
-interface IPatternCounterProps {
-    patternCount: string;   
+interface ICounterProps {
+    counterState: string;   
 }
 
-class PatternCounter extends React.Component<IPatternCounterProps> {
+class PatternCounter extends React.Component<ICounterProps> {
     public render() {
         return ( 
             <div className="counter">
                 <div className="count">
-                    <p>{this.props.patternCount}</p>
+                    <p>{this.props.counterState}</p>
                 </div>
                 <p>count</p>
             </div>
